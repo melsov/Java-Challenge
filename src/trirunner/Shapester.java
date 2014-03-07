a23f6653e2b52a359abee45155629e854242fdd5
melsov <melsovereign@yahoo.com>
2014-03-07 00:42:56 -0500

    shapester and trirunner

---
+++ src/trirunner/Shapester.java
@@ -0,0 +1,63 @@
+package trirunner;
+
+
+import java.awt.*;
+//import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+/* 
+ * This is like the FontDemo applet in volume 1, except that it 
+ * uses the Java 2D APIs to define and render the graphics and text.
+ */
+
+public class Shapester  
+{
+	//This is an 'enum'
+	public enum Mode {
+		SNAZZY, DULL, OUTRAGEOUS
+	}
+    
+    private double x = 0;
+    private double y = 0;
+    public double width = 50d;
+    public double height = 50d;
+    
+    
+    public double getx() {
+    	return x;
+    }
+    
+    public double gety(){
+    	return y;
+    }
+    
+    public void setx(double newX){
+    	x = newX;
+    }
+    
+    public void setY(double newY){
+    	y = newY;
+    }
+    
+    public void moveALittle()
+    {
+    	x += 0.1;
+    	y += 0.1;
+    }
+    
+    public Point2D.Double getTheFirstPoint()
+    {
+    	return new Point2D.Double(x,y);
+    }
+    
+    public Color getColor(){
+    	return Color.red;
+    }
+    
+    public Point.Double[] getPoints()
+    {
+    	return new Point2D.Double[] { new Point2D.Double(x + width, y),new Point2D.Double(x + width/2d, y + height),new Point2D.Double(x,y) };
+    }
+
+}
---
+++ src/trirunner/TriRunner.java
@@ -0,0 +1,126 @@
+package trirunner;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
+
+import javax.swing.Timer;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+
+
+/*
+ * Courtesy: http://zetcode.com/tutorials/javagamestutorial/
+ * 
+ */
+
+public class TriRunner extends JApplet implements ActionListener 
+{
+
+	/**
+	 * @param args
+	 */
+	
+	private static Timer timer;
+	
+	private Shapester shape1;
+	
+	
+	/*
+	 * Do me a favor and 
+	 * just blindly accept 
+	 * that we need this 
+	 * main function to get 
+	 * things rolling.
+	 */
+	public static void main(String[] args) 
+	{
+		JFrame f = new JFrame("Tri Runner");
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {System.exit(0);}
+        });
+        
+        JApplet applet = new TriRunner(); 
+        f.getContentPane().add("Center", applet);
+        applet.init();
+        
+        f.pack();
+        f.setSize(new Dimension(550,500));
+        f.setVisible(true);
+		
+		TriRunner tr = new TriRunner();
+		tr.startDoingStuff();
+	}
+	
+	public TriRunner()
+	{
+	}
+	
+	/*
+	 * Do me a favor:
+	 * just accept that
+	 * this function is called
+	 * at the start of the start of
+	 * everything when the program starts.
+	 */
+	public void init()
+	{
+		startDoingStuff();
+	}
+	
+	public void startDoingStuff()
+	{
+		shape1 = new Shapester();
+		
+		timer = new Timer(5, this);
+        timer.start();
+	}
+	
+	public void paint(Graphics graphics)
+	{
+		Graphics2D g = (Graphics2D) graphics;
+		g.clearRect(0, 0, 550, 500);
+		
+		drawAShapester(g, shape1);
+		
+	}
+	
+	private void drawAShapester(Graphics2D g, Shapester the_shapester)
+	{
+		setForeground(the_shapester.getColor());
+		
+		Point2D.Double firstPoint = the_shapester.getTheFirstPoint();
+		Point2D.Double[] points = the_shapester.getPoints();
+		GeneralPath line = new GeneralPath(GeneralPath.WIND_EVEN_ODD, points.length );
+		
+		line.moveTo(firstPoint.x, firstPoint.y);
+		
+		for (int i = 0; i < points.length ; ++i)
+		{
+			Point2D.Double nextPoint = points[i];
+			line.lineTo(nextPoint.x, nextPoint.y);
+		}
+		
+		g.fill(line);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent the_event) 
+	{
+	
+		shape1.moveALittle();
+		
+		
+		
+		repaint();
+		
+		
+	}
+
+}
